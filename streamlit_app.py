653fec1e283b9640a0789ee57e31b66c57eb4b42
vacentro19995 <136041450+vacentro19995@users.noreply.github.com>
2023-08-22 20:44:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ if streamlit.button('Get fruit load list'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
+#streamlit.stop()
 
 
 # Allow the end user to add a fruit to the list
